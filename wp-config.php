4a66467404648d566f8ee7170b48c439770ce21d
amolv <amolvhankalas@gmail.com>
2014-09-23 18:07:37 +0530

    gitignore

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'redcard');
+define('DB_NAME', 'redcard');	
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '10gXWOqeaf');
+define('DB_PASSWORD', 'toor');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
