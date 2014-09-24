36f305beee5e17037293b58ee70fd16725bec64d
amolv <amolvhankalas@gmail.com>
2014-09-24 16:51:42 +0530

    show CPT

---
+++ wp-content/themes/redcard/archive-show-articles.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * The template for displaying Archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each specific one. For example, Twenty Fourteen
+ * already has tag.php for Tag archives, category.php for Category archives,
+ * and author.php for Author archives.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			
+
+			<header class="page-header">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/radio_slide.jpg" class="tv_slide" />
+			</header><!-- .page-header -->
+			<div class="box">
+				<?php dynamic_sidebar( 'radioarchive' ); ?>	
+			</div>
+
+			<div class="box">
+				<h1>Radio Segments</h1>
+				<?php
+				$args = array( ' hide_empty = false ' );
+
+				$terms = get_terms('radio-categories', $args);
+			
+				if ( !empty( $terms ) && !is_wp_error( $terms ) ) {
+				    foreach ($terms as $term) {
+				    	echo '<div class="box">';
+				    	echo '<h4>' . $term->name. '</h4>';
+				    	$args = array(
+						'posts_per_page'   => 5,
+						'post_type'        => 'radio-articles',
+						'post_status'      => 'publish',
+						'tax_query' => array(
+								array(
+									'taxonomy' => 'radio-categories',
+									'field' => 'slug',
+									'terms' => $term->slug
+								)
+							)
+						);
+				    	$allarts = get_posts( $args );
+				    	$i = 1;
+				    	foreach ( $allarts as $article ) {	
+						   echo '<div class="r-child list'.$i.'">'. get_the_post_thumbnail( $article->ID ).'
+	   							<h3>'.$article->post_title.'</h3>
+	   							<a href="#">In Added Time</a>
+	   							<button>Listen</button>
+	   							<div id="social_2">
+	   								<a href="#"><div class="facebook"></div></a>
+	   								<a href="#"><div class="twitter"></div></a>
+	   								<a href="#"><div class="message"></div></a>
+	   							</div>
+	   							<span>1,290 views<span>
+	   						</span></span></div>';
+	   						$i++;
+						}
+				    	echo '<div class="lastl"><a href="#">View all in '.$term->name.'</a></div>';
+				        echo '</div>';
+				    }
+				  
+				}
+				?>
+			</div>
+			
+
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+<?php
+//get_sidebar( 'content' );
+//get_sidebar();
+get_footer();
--- wp-content/themes/redcard/inc/radio_artciles.php
+++ wp-content/themes/redcard/inc/radio_artciles.php
@@ -71,5 +71,71 @@ function codex_radioarticle_init() {
 	);
 
 	register_taxonomy( 'radio-categories', 'radio-articles', $args );
+
+
+	$labels = array(
+		'name'               => _x( 'Show Articles', 'post type general name', 'your-plugin-textdomain' ),
+		'singular_name'      => _x( 'Show Article', 'post type singular name', 'your-plugin-textdomain' ),
+		'menu_name'          => _x( 'Show Articles', 'admin menu', 'your-plugin-textdomain' ),
+		'name_admin_bar'     => _x( 'Show Article', 'add new on admin bar', 'your-plugin-textdomain' ),
+		'add_new'            => _x( 'Add New', 'Show Article', 'your-plugin-textdomain' ),
+		'add_new_item'       => __( 'Add New Show Article', 'your-plugin-textdomain' ),
+		'new_item'           => __( 'New Show Article', 'your-plugin-textdomain' ),
+		'edit_item'          => __( 'Edit Show Article', 'your-plugin-textdomain' ),
+		'view_item'          => __( 'View Show Article', 'your-plugin-textdomain' ),
+		'all_items'          => __( 'All Show Articles', 'your-plugin-textdomain' ),
+		'search_items'       => __( 'Search Show Articles', 'your-plugin-textdomain' ),
+		'parent_item_colon'  => __( 'Parent Show Articles:', 'your-plugin-textdomain' ),
+		'not_found'          => __( 'No Show Articles found.', 'your-plugin-textdomain' ),
+		'not_found_in_trash' => __( 'No Show Articles found in Trash.', 'your-plugin-textdomain' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		//'rewrite'            => array( 'slug' => '' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
+	register_post_type( 'show-articles', $args );
+
+	$labels = array(
+		'name'                       => _x( 'Show Categories', 'taxonomy general name' ),
+		'singular_name'              => _x( 'Show Category', 'taxonomy singular name' ),
+		'search_items'               => __( 'Search Show Categories' ),
+		'popular_items'              => __( 'Popular Show Categories' ),
+		'all_items'                  => __( 'All Show Categories' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Show Category' ),
+		'update_item'                => __( 'Update Show Category' ),
+		'add_new_item'               => __( 'Add New Show Category' ),
+		'new_item_name'              => __( 'New Show Category Name' ),
+		'separate_items_with_commas' => __( 'Separate Show Categories with commas' ),
+		'add_or_remove_items'        => __( 'Add or remove Show Categories' ),
+		'choose_from_most_used'      => __( 'Choose from the most used Show Categories' ),
+		'not_found'                  => __( 'No Show Categories found.' ),
+		'menu_name'                  => __( 'Show Categories' ),
+	);
+
+	$args = array(
+		'hierarchical'          => false,
+		'labels'                => $labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		//'rewrite'               => array( 'slug' => 'radio-categories' ),
+	);
+
+	register_taxonomy( 'show-categories', 'show-articles', $args );
 }
 
