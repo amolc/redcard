c741ae93371165f6027b23a694a95619cfe484d9
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-09-25 21:52:28 +0530

    Footer Changed again

--- wp-content/themes/redcard/footer.php
+++ wp-content/themes/redcard/footer.php
@@ -19,7 +19,7 @@
                 <h1>Home</h1>
                 <?php wp_nav_menu( array('menu' => 'Footer Mid' )); ?>
             </div>
-            
+              <div class="col_150">&nbsp;</div>
             <div class="col_300">
                 <?php dynamic_sidebar( 'footer-right' ); ?>
                 <div id="social"><?php dynamic_sidebar( 'header-social' ); ?></div>
