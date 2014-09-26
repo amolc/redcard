abffdd4865596f7486da18af62df87daecf70828
amaninfodreams <aman.infodreams@gmail.com>
2014-09-25 19:35:04 -0700

    redcard version 1.7.0
    set up page for radio single page

---
+++ wp-content/themes/redcard/sidebar-radio.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The Sidebar for Football
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+<div class="right">
+  <h2>Sponsor</h2>
+  <div class="ad_right"><?php dynamic_sidebar( 'postsponser' ); ?></div>
+  <h2>Popular Posts</h2>
+  <div class="popular">
+    <ul>
+      <?php /* For Popular Posts*/ if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('Sidebar 1') ) ?>
+     
+    </ul>
+  </div>
+</div>
--- wp-content/themes/redcard/single-radio-articles.php
+++ wp-content/themes/redcard/single-radio-articles.php
@@ -1,40 +1,26 @@
 <?php
 /**
  * The Template for displaying all single posts
- *
  * @package WordPress
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
  */
-
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<?php
-				// Start the Loop.
+  <div class="left">
+		<?php
 				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
-					// Previous/next post navigation.
-					twentyfourteen_post_nav();
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
+				//get_template_part( 'content', get_post_format() );
+		?>
+        <h1><?php the_title(); ?></h1>
+		<div class="single-post-image radio-single-image">
+        <?php 
+				//twentyfourteen_post_thumbnail();
+				
+				the_content();
+		?>
+      	</div>
+		<?php endwhile; ?>
+   </div>
 <?php
-//get_sidebar( 'content' );
-get_sidebar();
-get_footer();
+get_sidebar('radio');
+get_footer(); ?>
