ff4d76f425794c2731ecb7f00b1038a15c41a730
amaninfodreams <aman.infodreams@gmail.com>
2014-09-25 20:08:15 -0700

    redcard version 1.9.0
    single radio page setup

--- wp-content/themes/redcard/single-radio-articles.php
+++ wp-content/themes/redcard/single-radio-articles.php
@@ -12,6 +12,8 @@ get_header(); ?>
 				//get_template_part( 'content', get_post_format() );
 		?>
         <h1><?php the_title(); ?></h1>
+        <?php $all_meta = get_post_meta($post->ID);?>
+        <div class="single-rad-vid"><?php echo $all_meta['_oembed_f1323014c18094c603c874db4bb5064e'][0]; ?></div>
 		<div class="single-post-image radio-single-image">
         <?php 
 				//twentyfourteen_post_thumbnail();
