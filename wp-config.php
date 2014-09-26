9abf647cc201f6e588169378b6497a1a6d7ee268
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-09-26 14:59:52 +0530

    Advert Plugin v 1.1.0

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'redcard');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'toor');
+define('DB_PASSWORD', 'deep');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
---
+++ wp-content/plugins/advertisement/advertisement.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @package syonPolicy
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Red Card Advertisement
+Plugin URI: http://www.fountaintechies.com/
+Description: A direct way to add   advertisements.
+Author: Fountain Technologies
+Version: 1.0.0
+Author URI: http://www.fountaintechies.com/
+*/
+add_action('admin_menu', 'add_program_menu1');
+function add_program_menu1()
+{
+	add_menu_page(__('Advertisments','menu-test'), __('Advertisments','menu-test'), 'manage_options', 'manage-adverts', 'manage_Adverts' );
+	
+}
+register_activation_hook( __FILE__, 'myplugin_activate1' );
+
+function myplugin_activate1()
+{
+	global $wpdb;
+	 $tablename=$wpdb->prefix."adverts";
+	$mQuery="CREATE TABLE `$tablename` (
+  `adId` int(11) NOT NULL auto_increment,
+  `adimage` varchar(255) default NULL,
+  `adlink` text,
+  `isactive` enum('0','1') default '0',
+  PRIMARY KEY  (`adId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+";
+$mSql=$wpdb->query($mQuery);
+}
+function m_filter_text1($str)
+{
+	$str=ltrim(rtrim(strip_tags($str)));
+	return $str;
+}
+function manage_Adverts()
+{
+	?>
+        <style type="text/css">
+.mandatory {
+	color: red;
+}
+.maindiv {
+	width: 95%;
+	height: 100%;
+	padding: 20px;
+}
+</style>
+        <?php  $url = plugins_url();?>
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <link id="bs-css" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+        <link id="bsdp-css" href="<?php echo $url;?>/todayprogramme/datepicker3.css" rel="stylesheet">
+        <script type="text/javascript" src="<?php echo $url;?>/todayprogramme/bootstrap-datepicker.js"></script>
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<div class="box box-body">
+<br/><br/>
+<div class="col-md-6" style="background-color:#ebebeb;">
+	<form action="" method="post" enctype="multipart/form-data">
+    	<div class="form-group">
+        	<label>Advertisement image</label>
+        </div>
+    </form>
+</div>
+&nbsp;
+<div class="col-md-6"></div>
+</div>
+
+<?php
+	
+}
+?>
