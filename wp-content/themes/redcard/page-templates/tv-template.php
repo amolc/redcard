c1a656d32e68be97c1f44778535e08cb656bd6f9
amaninfodreams <aman.infodreams@gmail.com>
2014-09-26 20:04:28 -0700

    3.8.0
    title on tv

--- wp-content/themes/redcard/page-templates/tv-template.php
+++ wp-content/themes/redcard/page-templates/tv-template.php
@@ -81,13 +81,13 @@ return $newstr;
 					<div class="child">
 						<?php $videoID = ShowTvVideoImg($youtubURL_values,$alt = 'Video screenshot', $width='280', $height='150');// twentyfourteen_post_thumbnail( 'thumbnail', array( 'class' => 'video-feature-image' ) );?>
                         <a href="<?php the_permalink() ?>"><?php echo $videoID; ?></a>
-                        <?php $trimtitle = get_the_title();
-							  $shorttitle = wp_trim_words( $trimtitle, $num_words = 3, $more = '…' ); ?>
-						<a href="<?php the_permalink() ?>"><?php echo $shorttitle; ?></a>
+                        <?php //$trimtitle = get_the_title();
+							  //$shorttitle = wp_trim_words( $trimtitle, $num_words = 3, $more = '…' ); ?>
+						<a style="text-decoration: none;" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
                         <?php $youtubetagline_value = get_post_meta( $postID, '_cmb_tvideo_tagline_text', true ); ?>
-                        <?php $trimtag = $youtubetagline_value;
-							  $shorttag = wp_trim_words( $trimtag, $num_words = 10, $more = '…' ); ?>
-						<p class="tvpexcerpt"><?php echo $shorttag; ?></p>
+                        <?php //$trimtag = $youtubetagline_value;
+							  //$shorttag = wp_trim_words( $trimtag, $num_words = 10, $more = '…' ); ?>
+						<p class="tvpexcerpt"><?php echo $youtubetagline_value; ?></p>
 						<span></span>
 					</div>
 					<?php endwhile;?>
