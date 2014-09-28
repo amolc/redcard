97a121eb46682880fc891ef0a0c9374f8a850f28
amolv <amolvhankalas@gmail.com>
2014-09-28 20:03:03 +0530

    tv schedule

--- wp-content/themes/redcard/inc/tvvideo_shows.php
+++ wp-content/themes/redcard/inc/tvvideo_shows.php
@@ -1,24 +1,22 @@
 <?php
-
-
 add_action( 'init', 'tv_shows_init' );
 /* Register a football post type. */
 function tv_shows_init() {
 	$labels = array(
-		'name'               => _x( 'TV Shows ', 'post type general name', 'your-plugin-textdomain' ),
-		'singular_name'      => _x( 'TV Shows ', 'post type singular name', 'your-plugin-textdomain' ),
-		'menu_name'          => _x( 'TV Shows ', 'admin menu', 'your-plugin-textdomain' ),
-		'name_admin_bar'     => _x( 'TV Shows ', 'add new on admin bar', 'your-plugin-textdomain' ),
-		'add_new'            => _x( 'Add New', 'tv_shows', 'your-plugin-textdomain' ),
-		'add_new_item'       => __( 'Add New TV Shows  ', 'your-plugin-textdomain' ),
-		'new_item'           => __( 'New TV Shows  ', 'your-plugin-textdomain' ),
-		'edit_item'          => __( 'Edit', 'your-plugin-textdomain' ),
-		'view_item'          => __( 'View', 'your-plugin-textdomain' ),
-		'all_items'          => __( 'All TV Shows  ', 'your-plugin-textdomain' ),
-		'search_items'       => __( 'Search TV Shows ', 'your-plugin-textdomain' ),
-		'parent_item_colon'  => __( 'Parent TV Shows :', 'your-plugin-textdomain' ),
-		'not_found'          => __( 'No Post found.', 'your-plugin-textdomain' ),
-		'not_found_in_trash' => __( 'No Posts found in Trash.', 'your-plugin-textdomain' )
+		'name'               => _x( 'TV Schedules ', 'post type general name', 'your-plugin-textdomain' ),
+		'singular_name'      => _x( 'TV Schedule ', 'post type singular name', 'your-plugin-textdomain' ),
+		'menu_name'          => _x( 'TV Schedule ', 'admin menu', 'your-plugin-textdomain' ),
+		'name_admin_bar'     => _x( 'TV Schedule ', 'add new on admin bar', 'your-plugin-textdomain' ),
+		'add_new'            => _x( 'Add New TV Schedule', 'tv_Schedules', 'your-plugin-textdomain' ),
+		'add_new_item'       => __( 'Add New TV Schedule  ', 'your-plugin-textdomain' ),
+		'new_item'           => __( 'New TV Schedule', 'your-plugin-textdomain' ),
+		'edit_item'          => __( 'Edit TV Schedule', 'your-plugin-textdomain' ),
+		'view_item'          => __( 'View TV Schedule', 'your-plugin-textdomain' ),
+		'all_items'          => __( 'All TV Schedules', 'your-plugin-textdomain' ),
+		'search_items'       => __( 'Search TV Schedules', 'your-plugin-textdomain' ),
+		'parent_item_colon'  => __( 'Parent TV Schedule :', 'your-plugin-textdomain' ),
+		'not_found'          => __( 'No TV Schedule found.', 'your-plugin-textdomain' ),
+		'not_found_in_trash' => __( 'No TV Schedules found in Trash.', 'your-plugin-textdomain' )
 	);
 
 	$args = array(
@@ -28,7 +26,7 @@ function tv_shows_init() {
 		'show_ui'            => true,
 		'show_in_menu'       => true,
 		'query_var'          => true,
-		'rewrite'            => array( 'slug' => 'tv-show' ),
+		'rewrite'            => array( 'slug' => 'tv-schedule' ),
 		'capability_type'    => 'post',
 		'has_archive'        => true,
 		'hierarchical'       => false,
@@ -71,7 +69,7 @@ function register_taxonomy_tvshowcategory() {
     );
     
 
-   register_taxonomy( 'tvshowcategory', array('tvideoshow'), $args );
+   //register_taxonomy( 'tvshowcategory', array('tvideoshow'), $args );
 }
 
 
