bfce20b6009046cdf2fc48c304d948648c850772
amaninfodreams <aman.infodreams@gmail.com>
2014-09-29 19:58:08 +0530

    6.6.0
    search icon color

--- wp-content/themes/redcard/header.php
+++ wp-content/themes/redcard/header.php
@@ -87,7 +87,7 @@
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 	</label>
-	<button type="submit" class="search-submit" style="width:30px;" ><i class="fa fa-search"></i></button>
+	<button type="submit" class="search-submit" style="width:30px;background: none repeat scroll 0 0 black;border: medium none;" ><i class="fa fa-search" style="color: #fff;"></i></button>
 </form>
       </li>
     </ul>
