0f33fc5fba6a7b8d9a7aeacfddb4985b1ec3d188
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-09-29 11:35:22 +0530

    Point 11-12 (Image / Comments)

--- wp-content/themes/redcard/header.php
+++ wp-content/themes/redcard/header.php
@@ -68,7 +68,7 @@
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( '<i class="fa fa-search"></i>', 'submit button' ) ?>" />
+	<button type="submit" class="search-submit" ><?php echo esc_attr_x( '<i class="fa fa-search"></i>', 'submit button' ) ?></button>
 </form>
       </li>
     </ul>
