8ab47325ad888023984fa2ec932d2971e762bf9b
amaninfodreams <aman.infodreams@gmail.com>
2014-09-30 20:43:33 +0530

    8.3.0

--- wp-content/themes/redcard/page-templates/football-template.php
+++ wp-content/themes/redcard/page-templates/football-template.php
@@ -21,8 +21,88 @@ get_header();
 				  <a href="<?php the_permalink() ?>">
                       <h1><?php the_title(); ?></h1>
                       </a>
-                      <div class="date"> <a href="#">Singapore</a> <span><?php the_time('l, F j, Y'); ?></span>
-                        <div id="social_3">
+                     
+                     
+                     	<?php $postID = get_the_ID(); 
+						
+						   $mytermArray = array();
+                          $term_list_reg = wp_get_post_terms($postID, 'footballregions');  
+						  $term_list_exc = wp_get_post_terms($postID, 'footballexclusive'); 
+						  $term_list_opi = wp_get_post_terms($postID, 'footballopinion');  
+						  $term_list_week = wp_get_post_terms($postID, 'footballintheweek'); 
+						  $as =1;
+						
+						  if(!empty($term_list_reg))
+						  {
+							  foreach($term_list_reg as $row)
+							  {
+								  $mytermArray[$as]['term_id']= $row->term_id;
+								  $mytermArray[$as]['name']= $row->name;
+								   $mytermArray[$as]['slug']= $row->slug;
+								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
+								   $mytermArray[$as]['link']= get_term_link( $row );
+								 
+								  $as++;
+							  }
+						  }
+						  if(!empty($term_list_exc))
+						  {
+							  foreach($term_list_exc as $row)
+							  {
+								  $mytermArray[$as]['term_id']= $row->term_id;
+								  $mytermArray[$as]['name']= $row->name;
+								   $mytermArray[$as]['slug']= $row->slug;
+								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
+								   $mytermArray[$as]['link']= get_term_link( $row );
+								  $as++;
+							  }
+						  }
+						  if(!empty($term_list_opi))
+						  {
+							  foreach($term_list_opi as $row)
+							  {
+								  $mytermArray[$as]['term_id']= $row->term_id;
+								  $mytermArray[$as]['name']= $row->name;
+								   $mytermArray[$as]['slug']= $row->slug;
+								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
+								   $mytermArray[$as]['link']= get_term_link( $row );
+								  $as++;
+							  }
+						  }
+						   if(!empty($term_list_week))
+						  {
+							  foreach($term_list_week as $row)
+							  {
+								  $mytermArray[$as]['term_id']= $row->term_id;
+								  $mytermArray[$as]['name']= $row->name;
+								   $mytermArray[$as]['slug']= $row->slug;
+								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
+								   $mytermArray[$as]['link']= get_term_link( $row );
+								  $as++;
+							  }
+						  }
+						 
+						    ?> 
+            
+            
+                      <div class="date">
+                      <?php if(!empty($mytermArray)){
+						  $is =1;
+						 $arraycount =  count($mytermArray);
+						  foreach($mytermArray as $row){
+							  
+							
+							  	if($is == $arraycount)
+								{
+						  ?>
+                       <a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>
+                      <?php }else{ ?> 
+					    <a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>,
+					  <?php }$is++; }}?>
+                     
+					 
+                      <span><?php the_time('l, F j, Y'); ?></span>
+                      <div id="social_3">
                           <a onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;" href="http://www.facebook.com/share.php?u=<?php echo urlencode(get_permalink( $article->ID));?>"  title="Share on Facebook" ><div class="facebook" ></div></a>
          <a href="http://twitter.com/intent/tweet?text= <?php the_title(); ?> <?php echo get_permalink( $article->ID);?> via @RedCardConnect&url="   onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;"><div class="twitter"></div></a>
         <a onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;"  target="_blank" href="mailto:?subject=<?php echo get_permalink( $article->ID);?>&body=<?php echo get_permalink( $article->ID);?>"><div class="message"></div></a>
@@ -37,6 +117,8 @@ get_header();
                         <ul>
       
 			<?php } if($as > 1) { ?>
+            
+			
             
             <li>
  				<div class="img"> <?php twentyfourteen_post_thumbnail( 'thumbnail', array( 'class' => 'post-feature-image' ) );?> <span>Singapore</span> </div>
