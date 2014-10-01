2a025f4ff07ff8d968f27a81be2bf034945cc313
amaninfodreams <aman.infodreams@gmail.com>
2014-10-01 12:03:46 +0530

    8.4.1

--- wp-content/themes/redcard/page-templates/football-template.php
+++ wp-content/themes/redcard/page-templates/football-template.php
@@ -9,23 +9,21 @@ get_header();
 
 <div class="left">
   <h2>Football</h2>
-
-      
-		<?php 
+  <?php 
 		
 			$args = array( 'post_type' => 'footballs');
-			$loop = new WP_Query( $args ); $as =1; 
+			$loop = new WP_Query( $args ); $ad =1; 
 			while ( $loop->have_posts() ) : $loop->the_post();
 		
-			if($as == 1){?>
-				  <a href="<?php the_permalink() ?>">
-                      <h1><?php the_title(); ?></h1>
-                      </a>
-                     
-                     
-                     	<?php $postID = get_the_ID(); 
+			if($ad == 1){?>
+  <a href="<?php the_permalink() ?>">
+  <h1>
+    <?php the_title(); ?>
+  </h1>
+  </a>
+  <?php $postID = get_the_ID(); 
 						
-						   $mytermArray = array();
+						  $mytermArray = array();
                           $term_list_reg = wp_get_post_terms($postID, 'footballregions');  
 						  $term_list_exc = wp_get_post_terms($postID, 'footballexclusive'); 
 						  $term_list_opi = wp_get_post_terms($postID, 'footballopinion');  
@@ -38,9 +36,9 @@ get_header();
 							  {
 								  $mytermArray[$as]['term_id']= $row->term_id;
 								  $mytermArray[$as]['name']= $row->name;
-								   $mytermArray[$as]['slug']= $row->slug;
+								  $mytermArray[$as]['slug']= $row->slug;
 								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
-								   $mytermArray[$as]['link']= get_term_link( $row );
+								  $mytermArray[$as]['link']= get_term_link( $row );
 								 
 								  $as++;
 							  }
@@ -51,9 +49,9 @@ get_header();
 							  {
 								  $mytermArray[$as]['term_id']= $row->term_id;
 								  $mytermArray[$as]['name']= $row->name;
-								   $mytermArray[$as]['slug']= $row->slug;
+								  $mytermArray[$as]['slug']= $row->slug;
 								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
-								   $mytermArray[$as]['link']= get_term_link( $row );
+								  $mytermArray[$as]['link']= get_term_link( $row );
 								  $as++;
 							  }
 						  }
@@ -63,9 +61,9 @@ get_header();
 							  {
 								  $mytermArray[$as]['term_id']= $row->term_id;
 								  $mytermArray[$as]['name']= $row->name;
-								   $mytermArray[$as]['slug']= $row->slug;
+								  $mytermArray[$as]['slug']= $row->slug;
 								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
-								   $mytermArray[$as]['link']= get_term_link( $row );
+								  $mytermArray[$as]['link']= get_term_link( $row );
 								  $as++;
 							  }
 						  }
@@ -75,64 +73,59 @@ get_header();
 							  {
 								  $mytermArray[$as]['term_id']= $row->term_id;
 								  $mytermArray[$as]['name']= $row->name;
-								   $mytermArray[$as]['slug']= $row->slug;
+								  $mytermArray[$as]['slug']= $row->slug;
 								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
-								   $mytermArray[$as]['link']= get_term_link( $row );
+								  $mytermArray[$as]['link']= get_term_link( $row );
 								  $as++;
 							  }
 						  }
 						 
-						    ?> 
-            
-            
-                      <div class="date">
-                      <?php if(!empty($mytermArray)){
-						  $is =1;
+						    ?>
+  <div class="date">
+    <?php if(!empty($mytermArray)){
+						 $is =1;
 						 $arraycount =  count($mytermArray);
 						  foreach($mytermArray as $row){
-							  
-							
 							  	if($is == $arraycount)
 								{
-						  ?>
-                       <a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>
-                      <?php }else{ ?> 
-					    <a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>,
-					  <?php }$is++; }}?>
-                     
-					 
-                      <span><?php the_time('l, F j, Y'); ?></span>
-                      <div id="social_3">
-                          <a onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;" href="http://www.facebook.com/share.php?u=<?php echo urlencode(get_permalink( $article->ID));?>"  title="Share on Facebook" ><div class="facebook" ></div></a>
-         <a href="http://twitter.com/intent/tweet?text= <?php the_title(); ?> <?php echo get_permalink( $article->ID);?> via @RedCardConnect&url="   onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;"><div class="twitter"></div></a>
-        <a onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;"  target="_blank" href="mailto:?subject=<?php echo get_permalink( $article->ID);?>&body=<?php echo get_permalink( $article->ID);?>"><div class="message"></div></a>
-        
-                        </div>
-                      </div>
+	?>
+    							<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>
+    							<?php } else { ?>
+    											<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>,
+    									<?php } ?>
+                                <?php $is++; ?>
+                                <?php } } ?>
 
-                      <div class="footballs-top-image-latest"><?php twentyfourteen_post_thumbnail();?></div>
-                      <div class="f_text"><?php echo get_the_excerpt(); ?>  <a href="<?php the_permalink() ?>">Read More</a> </div>
-                      
-                      <div class="c_list">
-                        <ul>
-      
-			<?php } if($as > 1) { ?>
-            
-			
-            
-            <li>
- 				<div class="img"> <?php twentyfourteen_post_thumbnail( 'thumbnail', array( 'class' => 'post-feature-image' ) );?> <span>Singapore</span> </div>
-
-     			<div class="text">
-                    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-                    <p><?php echo get_the_excerpt(); ?>  <a href="<?php the_permalink() ?>">Read More</a></p>
-				</div>
-                </li>
-			<?php } $as++; endwhile;?>
-      
+    
+    <span><?php the_time('l, F j, Y'); ?></span>
+    <div id="social_3"> <a onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;" href="http://www.facebook.com/share.php?u=<?php echo urlencode(get_permalink( $article->ID));?>"  title="Share on Facebook" >
+      <div class="facebook" ></div>
+      </a> <a href="http://twitter.com/intent/tweet?text= <?php the_title(); ?> <?php echo get_permalink( $article->ID);?> via @RedCardConnect&url="   onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;">
+      <div class="twitter"></div>
+      </a> <a onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;"  target="_blank" href="mailto:?subject=<?php echo get_permalink( $article->ID);?>&body=<?php echo get_permalink( $article->ID);?>">
+      <div class="message"></div>
+      </a> </div>
+  </div>
+  <div class="footballs-top-image-latest">
+    <?php twentyfourteen_post_thumbnail();?>
+  </div>
+  <div class="f_text"><?php echo get_the_excerpt(); ?> <a href="<?php the_permalink() ?>">Read More</a> </div>
+  <div class="c_list">
+    <ul>
+      <?php } if($ad > 1) { ?>
+      <li>
+        <div class="img">
+          <?php twentyfourteen_post_thumbnail( 'thumbnail', array( 'class' => 'post-feature-image' ) );?>
+          <span>Singapore</span> </div>
+        <div class="text"> <a href="<?php the_permalink() ?>">
+          <?php the_title(); ?>
+          </a>
+          <p><?php echo get_the_excerpt(); ?> <a href="<?php the_permalink() ?>">Read More</a></p>
+        </div>
+      </li>
+      <?php } $ad++; endwhile;?>
     </ul>
   </div>
 </div>
 <?php get_sidebar('football');?>
-
 <?php get_footer();?>
