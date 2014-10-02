0197b78d533f2e9d3f4892b7537b0c1f1bea0a5c
amaninfodreams <aman.infodreams@gmail.com>
2014-10-02 13:46:14 +0530

    9.8.2

--- wp-content/themes/redcard/archive-radioshowsartcles.php
+++ wp-content/themes/redcard/archive-radioshowsartcles.php
@@ -23,7 +23,7 @@ get_header(); ?>
         <div class="text"> <a href="<?php the_permalink() ?>">
           <?php the_title(); ?>
           </a>
-          <p><?php echo get_the_excerpt(); ?> <a href="<?php the_permalink() ?>">Read More</a></p>
+          <p><?php echo get_the_excerpt(); ?></p>
         </div>
       </li>
       <?php  $ad++; endwhile;?>
