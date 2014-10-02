b886f19adc464934d223b8fdcc899cdebca268f4
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-10-02 21:57:46 +0530

    Archive Radio Articles

--- wp-content/themes/redcard/archive-radio-articles.php
+++ wp-content/themes/redcard/archive-radio-articles.php
@@ -46,8 +46,7 @@ get_header();
 	   							echo '<a href="'.get_permalink( $article->ID).'" class="llink">Listen</a>
 	   							<div id="social_2">';
 								?>
-								  <a onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;" href="http://www.facebook.com/share.php?u=
-								  [title]=<?php echo $radtitle;?>&[url]=<?php echo get_permalink( $article->ID);?> via @RedCardConnect"  title="Share on Facebook" ><div class="facebook" ></div></a>
+								  <a onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;" href="http://www.facebook.com/share.php?u=[title]=<?php echo $radtitle;?>&[url]=<?php echo get_permalink( $article->ID);?> via @RedCardConnect"  title="Share on Facebook" ><div class="facebook" ></div></a>
        <a href="http://twitter.com/intent/tweet?text=<?php echo $radtitle. get_permalink( $article->ID);?> via @RedCardConnect&url="   onclick="javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;"><div class="twitter"></div></a>
         <a href="<?php echo get_permalink( $article->ID);?>#dis_comment"><div class="message"></div></a>
         
