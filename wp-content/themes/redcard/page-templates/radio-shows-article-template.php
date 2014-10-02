09c4b24b4145777a5abf01b7dd6f93e2b7bdb2e6
amaninfodreams <aman.infodreams@gmail.com>
2014-10-02 15:38:03 +0530

    9.9.0

--- wp-content/themes/redcard/page-templates/radio-shows-article-template.php
+++ wp-content/themes/redcard/page-templates/radio-shows-article-template.php
@@ -8,30 +8,39 @@ get_header();
 <div class="left">
   <h2>Radio Shows Articles</h2>
   <?php 
-		
 			$args = array( 'post_type' => 'radioshowsartcles');
-			$loop = new WP_Query( $args ); 
+			$loop = new WP_Query( $args ); $sd=0;
 			while ( $loop->have_posts() ) : $loop->the_post();
+			 $selectsql = "SELECT *  FROM `rd_postmeta` WHERE `meta_key` = '_cmb_radio_shows_articles_url'";
+			 
+         	 $resultarray = $wpdb->get_results($selectsql) ;
+
+			 if(!empty($resultarray)){
+				$URL_values="";
+				$URL_values = get_post_meta( $resultarray[$sd]->post_id, '_cmb_radio_shows_articles_url', true ); 
+				if(!$URL_values)
+				{
+					$URL_values="#";
+				}
 ?>
   <div class="c_list radio-showscss">
     <ul>
-
       <li>
         <div class="img">
           <?php twentyfourteen_post_thumbnail( 'thumbnail', array( 'class' => 'post-feature-image' ) );?>
           </div>
-        <div class="text"> <a href="<?php the_permalink() ?>">
+       <?php ?>
+        <div class="text"> <a href="<?php echo $URL_values; ?>">
           <?php the_title(); ?>
           </a>
           <p><?php echo get_the_excerpt(); ?></p>
         </div>
+        <?php
+		?>
       </li>
-      
     </ul>
-    
   </div>
-  <?php   endwhile;?>
-
+  <?php   } $sd++; endwhile; ?>
 </div>
 <?php get_sidebar('football');?>
 <?php get_footer();?>
