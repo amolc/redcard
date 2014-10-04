04ce75e7f007ff0e8c0019b2193432f743df8710
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-10-04 14:43:53 +0530

    Advertisments Applied on every where

--- wp-content/themes/redcard/page-templates/football-opinion.php
+++ wp-content/themes/redcard/page-templates/football-opinion.php
@@ -139,5 +139,37 @@ get_header();
 </div>
 
 
-<?php get_sidebar('football');?>
+<div class="right">
+ 
+  <?php
+  $m_table=$wpdb->prefix."adverts";
+	$advertQuery="select * from $m_table where page='football-article' and isactive='1' order by adId DESC LIMIT 0,1";
+	$advertSql=$wpdb->get_results($advertQuery);
+	
+	if(sizeof($advertSql)>0)
+	{
+		foreach($advertSql as $adsql)
+		{
+			?>
+			 <h2>ADVERTISEMENT</h2>
+			  <div class="ad_right" align="center">
+              <a href="<?php echo urldecode($adsql->adlink2);?>" target="_blank"><img width="302" height="252" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage2;?>" /></a>
+              <label style="font-size:10px;">Advertisement</label>
+              </div>
+			<?php
+		}
+	}
+		?>
+  
+  
+  <h2>Popular Posts</h2>
+  <div class="popular">
+    <ul>
+      <?php /* For Popular Posts*/ if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('Sidebar 1') ) ?>
+     
+    </ul>
+  </div>
+ 
+</div>
+
 <?php get_footer();?>
