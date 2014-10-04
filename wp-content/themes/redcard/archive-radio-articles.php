6f0ea9d2582398dd89a7ec43c8a92fa0b151e572
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-10-04 15:59:44 +0530

    Point no 42

--- wp-content/themes/redcard/archive-radio-articles.php
+++ wp-content/themes/redcard/archive-radio-articles.php
@@ -45,9 +45,12 @@ $m_table=$wpdb->prefix."adverts";
 			$postID = get_the_ID(); 
 		   	echo '<div class="r-child list'.$i.'">'. get_the_post_thumbnail( $postID ).'
 			<h3><a href="'.get_permalink( $postID).'" class="r-child-h3-a child-a-left">'.get_the_title().'</a></h3>';
-			$term_list_reg = wp_get_post_terms($postID, 'radio-shows');
-		    $mlink=(get_term_link( $term_list_reg[0] ));
-			echo "<a href='".$mlink."'>".$term_list_reg[0]->name."</a>";
+			 $term_list_reg = wp_get_post_terms($postID, 'radio-shows');
+      					   $mlink=(get_term_link( $term_list_reg[0] ));
+						   $term_list_reg1 = wp_get_post_terms($postID, 'radio-categories');
+      					   $mlink1=(get_term_link( $term_list_reg1[0] ));
+			echo '<a href="'.$mlink.'" style="margin-bottom:0px !important;">'.$term_list_reg[0]->name.':</a>  
+								<a href="'.$mlink1.'">'.$term_list_reg1[0]->name.'</a>';
 	   		echo '<a href="'.get_permalink( $postID).'" class="llink">Listen</a><div id="social_2">';
 	?>		
 			<a onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;" href="http://www.facebook.com/share.php?u=<?php echo get_permalink($postID);?>&description=<?php echo get_the_title();?>"  title="Share on Facebook" >
