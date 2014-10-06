343566a123a1a135176dc3bb8a69f84876ffcd78
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-10-06 14:46:03 +0530

    other sports

--- wp-content/themes/redcard/page-templates/other-sports-template.php
+++ wp-content/themes/redcard/page-templates/other-sports-template.php
@@ -3,6 +3,23 @@
  * Template Name: Other Sports template Page
  */
 get_header(); 
+$m_table=$wpdb->prefix."adverts";
+	$advertQuery="select * from $m_table where page='other-sports' and isactive='1' order by adId DESC LIMIT 0,1";
+	$advertSql=$wpdb->get_results($advertQuery);
+	
+	if(sizeof($advertSql)>0)
+	{
+		foreach($advertSql as $adsql)
+		{
+		?>
+		
+<div class="ad_1" align="center"><a href="<?php echo urldecode($adsql->adlink1);?>" target="_blank"><img width="731" height="93" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage1;?>" /></a>
+<label style="font-size:9px;">ADVERTISEMENT</label>
+</div>
+
+		<?php
+		}
+	}
 ?>
 
 <div class="left">
