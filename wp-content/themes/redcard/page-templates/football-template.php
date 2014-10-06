18c5255a9b18d6f33672e8ca1d230e8240cf534f
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-10-06 13:10:45 +0530

    Football Front Page With Advertisement

--- wp-content/themes/redcard/page-templates/football-template.php
+++ wp-content/themes/redcard/page-templates/football-template.php
@@ -4,9 +4,25 @@
  
  */
 get_header(); 
+  $m_table=$wpdb->prefix."adverts";
+	$advertQuery="select * from $m_table where page='football-articles' and isactive='1' order by adId DESC LIMIT 0,1";
+	$advertSql=$wpdb->get_results($advertQuery);
+	
 
 ?>
+<?php if(sizeof($advertSql)>0)
+	{
+		foreach($advertSql as $adsql)
+		{
+		?>
+		
+<div class="a d_1" align="center"><a href="<?php echo urldecode($adsql->adlink2);?>" target="_blank"><img width="302" height="252" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage2;?>" /></a>
+<label style="font-size:10px;">Advertisement</label>
+</div>
 
+		<?php
+		}
+	} ?>
 <div class="left">
   <h2>Football</h2>
   <?php 
@@ -154,10 +170,6 @@ get_header();
 <div class="right">
  
   <?php
-  $m_table=$wpdb->prefix."adverts";
-	$advertQuery="select * from $m_table where page='football-article' and isactive='1' order by adId DESC LIMIT 0,1";
-	$advertSql=$wpdb->get_results($advertQuery);
-	
 	if(sizeof($advertSql)>0)
 	{
 		foreach($advertSql as $adsql)
