005026b2277544dfeeaf39c84e631b29383b32ce
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-10-06 14:19:28 +0530

    Football Adverts in menus

--- wp-content/themes/redcard/page-templates/football-exclusive.php
+++ wp-content/themes/redcard/page-templates/football-exclusive.php
@@ -4,8 +4,26 @@
  
  */
 get_header(); 
+	$m_table=$wpdb->prefix."adverts";
+	$advertQuery="select * from $m_table where page='football-exclusive' and isactive='1' order by adId DESC LIMIT 0,1";
+	$advertSql=$wpdb->get_results($advertQuery);
+	
+	if(sizeof($advertSql)>0)
+	{
+		foreach($advertSql as $adsql)
+		{
+		?>
+		
+<div class="ad_1" align="center"><a href="<?php echo urldecode($adsql->adlink1);?>" target="_blank"><img width="731" height="93" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage1;?>" /></a>
+<label style="font-size:9px;">ADVERTISEMENT</label>
+</div>
+
+		<?php
+		}
+	}
 
 ?>
+
 
 <div class="left">
   <h2>Football > Exclusive</h2>
@@ -155,7 +173,7 @@ get_header();
  
   <?php
   $m_table=$wpdb->prefix."adverts";
-	$advertQuery="select * from $m_table where page='football-article' and isactive='1' order by adId DESC LIMIT 0,1";
+	$advertQuery="select * from $m_table where page='football-exclusive' and isactive='1' order by adId DESC LIMIT 0,1";
 	$advertSql=$wpdb->get_results($advertQuery);
 	
 	if(sizeof($advertSql)>0)
--- wp-content/themes/redcard/page-templates/football-intheweek.php
+++ wp-content/themes/redcard/page-templates/football-intheweek.php
@@ -5,6 +5,24 @@
  */
 get_header(); 
 
+$m_table=$wpdb->prefix."adverts";
+	$advertQuery="select * from $m_table where page='football-intheweek' and isactive='1' order by adId DESC LIMIT 0,1";
+	$advertSql=$wpdb->get_results($advertQuery);
+	
+	if(sizeof($advertSql)>0)
+	{
+		foreach($advertSql as $adsql)
+		{
+		?>
+		
+<div class="ad_1" align="center"><a href="<?php echo urldecode($adsql->adlink1);?>" target="_blank"><img width="731" height="93" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage1;?>" /></a>
+<label style="font-size:9px;">ADVERTISEMENT</label>
+</div>
+
+		<?php
+		}
+	}
+
 ?>
 
 
@@ -157,7 +175,7 @@ get_header();
  
   <?php
   $m_table=$wpdb->prefix."adverts";
-	$advertQuery="select * from $m_table where page='football-article' and isactive='1' order by adId DESC LIMIT 0,1";
+	$advertQuery="select * from $m_table where page='football-intheweek' and isactive='1' order by adId DESC LIMIT 0,1";
 	$advertSql=$wpdb->get_results($advertQuery);
 	
 	if(sizeof($advertSql)>0)
