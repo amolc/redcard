ad8636373a603f1b4869428bafbe1afef6627c3c
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-10-06 12:34:18 +0530

    Tv Show List Template

--- wp-content/themes/redcard/page-templates/tv-shows-list-template.php
+++ wp-content/themes/redcard/page-templates/tv-shows-list-template.php
@@ -4,7 +4,31 @@
 * */
 get_header(); 
 ?>
-<?php dynamic_sidebar( 'tvshowsbanner' ); ?>
+<?php dynamic_sidebar( 'tvshowsbanner' ); $m_table=$wpdb->prefix."adverts";
+	$advertQuery="select * from $m_table where page='tv-shows' and isactive='1' order by adId DESC LIMIT 0,1";
+	$advertSql=$wpdb->get_results($advertQuery);
+	
+	if(sizeof($advertSql)>0)
+	{
+		
+		foreach($advertSql as $adsql)
+		{
+			if($adsql->adlink1)
+			{
+			
+		?>
+		
+<div class="ad_1" align="center"><a href="<?php echo urldecode($adsql->adlink1);?>" target="_blank"><img width="731" height="93" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage1;?>" /></a>
+<label style="font-size:9px;">ADVERTISEMENT</label>
+</div>
+
+		<?php
+			}
+		}
+	}
+
+
+?>
 <div class="left">
   <h2>TV Shows</h2>
 	<?php 
