1d41f97302fb20fdcfadba4e2a90655d9759c467
Infodreams Solutions <infodreamssolutions@gmaik.com>
2014-10-06 21:34:43 +0530

    Header.php changes

--- wp-content/themes/redcard/header.php
+++ wp-content/themes/redcard/header.php
@@ -208,7 +208,7 @@ if(is_front_page()){
 <div class="box">
   <div class="left">
     <h1 class="h1">Latest News</h1>
-    <?php $args = array( 'post_type' => 'footballs','order' => 'DESC','numberposts' => 6);
+    <?php $args = array( 'post_type' => 'footballs','order' => 'DESC','posts_per_page'   => 6);
 	  $loop = new WP_Query( $args ); $as =1;?>
     <?php	while ( $loop->have_posts() ) : $loop->the_post(); ?>
     <div id="l_n" class="home-latest-image" style="height:420px;">
