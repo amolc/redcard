c4068938e83edb82cc5b5d0859d1569ef97274bc
amaninfodreams <aman.infodreams@gmail.com>
2014-10-07 20:14:32 +0530

    14.5.0

--- wp-content/themes/redcard/archive-radio-articles.php
+++ wp-content/themes/redcard/archive-radio-articles.php
@@ -25,20 +25,89 @@ $m_table=$wpdb->prefix."adverts";
 		<?php
 		}
 	}
+	$param_1 = "";
+		if(isset($_GET['param']))
+		{
+			$param_1=$_GET['param'];
+		}
 	?>
 <div class="box">
-	<h1>Latest Radio Shows </h1>
+	<h1>Latest Radio Shows 
+    <div class="redsort">
+    	<form>
+            <select name="dropdown" id="sel"  onclick="changeOpt()">
+                <option value="latest" <?php if($param_1 == "latest" ){ echo 'selected="selected"';} ?> >Latest</option>
+                <option value="bycategories" <?php if($param_1 == "bycategories" ){ echo 'selected="selected"';} ?> >By Categories</option>
+                <option value="byshows" <?php if($param_1 == "byshows" ){ echo 'selected="selected"';} ?> >By Shows</option>
+            </select>
+        </form>
+<script>
+function changeOpt(){
+  document.getElementById("sel");
+  window.location="?param="+ document.getElementById("sel").value;
+}
+</script>
+    </div></h1>
 		<?php
 		echo '<div class="box">';
 		echo '<h4>' . $term->name. '</h4>';
 		$current_page = (get_query_var('paged')) ? get_query_var('paged') : 1;
-		$args = array(
+		
+		if($param_1=="bycategories")
+			{
+
+				$categories = get_terms( 'radio-categories', array('orderby' => 'count', 'hide_empty' => 1) );
+				$arrcat =  array();										 
+				if(!empty($categories))
+				{
+					foreach($categories as $catId)
+						{
+							$arrcat[]= $catId->slug;
+						}
+				}
+				$am = implode(',', $arrcat);
+				
+				$args = array(
+							'posts_per_page'   => 10,
+							'post_type'        => 'radio-articles',
+							'radio-categories' => $am,
+							'order'			=> 'ASC',
+							'post_status'      => 'publish',
+							'paged' 			=> $current_page,
+							);	
+			}
+		elseif($param_1=="byshows")
+			{
+				$categories = get_terms( 'radio-shows', array('orderby' => 'count', 'hide_empty' => 1) );
+				$arrcat =  array();										 
+				if(!empty($categories))
+				{
+					foreach($categories as $catId)
+						{
+							$arrcat[]= $catId->slug;
+						}
+				}
+				$am = implode(',', $arrcat);
+				
+				$args = array(
+							'posts_per_page'   => 10,
+							'post_type'        => 'radio-articles',
+							'radio-shows' 	  => $am,
+							'order'			=> 'ASC',
+							'post_status'      => 'publish',
+							'paged' 			=> $current_page,
+							);			
+			}
+		else
+		{
+			$args = array(
 						'posts_per_page'   => 10,
 						'post_type'        => 'radio-articles',
 						'post_status'      => 'publish',
                         'paged' 			=>  $current_page,
 					);
-                                        
+		}
+		
         $i = 1;
 		$loop = new WP_Query( $args );
 		while ( $loop->have_posts() ) : $loop->the_post();
@@ -46,9 +115,15 @@ $m_table=$wpdb->prefix."adverts";
 		   	echo '<div class="r-child list'.$i.'">'. get_the_post_thumbnail( $postID ).'
 			<h3><a href="'.get_permalink( $postID).'" class="r-child-h3-a child-a-left">'.get_the_title().'</a></h3>';
 			 $term_list_reg = wp_get_post_terms($postID, 'radio-shows');
-      					   $mlink=(get_term_link( $term_list_reg[0] ));
-						   $term_list_reg1 = wp_get_post_terms($postID, 'radio-categories');
-      					   $mlink1=(get_term_link( $term_list_reg1[0] ));
+			 if(!empty($term_list_reg))
+			 {
+      			   $mlink=(get_term_link( $term_list_reg[0] ));
+			 }
+			 $term_list_reg1 = wp_get_post_terms($postID, 'radio-categories');
+			 if(!empty($term_list_reg1))
+			 {
+      			   $mlink1=(get_term_link( $term_list_reg1[0] ));
+			 }
 			echo '<a href="'.$mlink.'" style="margin-bottom:0px !important;">'.$term_list_reg[0]->name.':</a>  
 								<a href="'.$mlink1.'">'.$term_list_reg1[0]->name.'</a>';
 	   		echo '<a href="'.get_permalink( $postID).'" class="llink">Listen</a><div id="social_2">';
@@ -67,9 +142,50 @@ $m_table=$wpdb->prefix."adverts";
 			echo '</div>';
 ?>
 <?php
-      if (function_exists(custom_pagination)) {
-        custom_pagination($loop->max_num_pages,"",$current_page);
-      }
+echo get_pagenum_link(3);
+
+function custom_pagination_r($numpages = '', $pagerange = '', $paged='') {
+   if (empty($pagerange)) {
+    $pagerange = 2;
+  }
+  global $paged;
+  if (empty($paged)) {
+    $paged = 1;
+  }
+  if ($numpages == '') {
+    global $wp_query;
+    $numpages = $wp_query->max_num_pages;
+    if(!$numpages) {
+        $numpages = 1;
+    }
+  }
+  $pagination_args = array(
+    'base'            => get_pagenum_link(2) . '%_%',
+    'format'          => 'page/%#%',
+    'total'           => $numpages,
+    'current'         => $paged,
+    'show_all'        => False,
+    'end_size'        => 1,
+    'mid_size'        => $pagerange,
+    'prev_next'       => True,
+    'prev_text'       => __('&laquo;'),
+    'next_text'       => __('&raquo;'),
+    'type'            => 'plain',
+    'add_args'        => false,
+    'add_fragment'    => ''
+  );
+  $paginate_links = paginate_links($pagination_args);
+  if ($paginate_links) {
+    echo "<nav class='custom-pagination'>";
+      echo "<span class='page-numbers page-num'>Page " . $paged . " of " . $numpages . "</span> ";
+      echo $paginate_links;
+    echo "</nav>";
+  }
+}
+
+      
+        custom_pagination_r($loop->max_num_pages,"",$current_page);
+      
 ?>
 </div>
 <?php get_footer();?>
