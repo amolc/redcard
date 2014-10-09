14d6ab8cd50fcde1a57c19c0b7535fc252a3243d
amaninfodreams <aman.infodreams@gmail.com>
2014-10-09 15:14:01 +0530

    15.4

--- wp-content/themes/redcard/archive-radio-articles.php
+++ wp-content/themes/redcard/archive-radio-articles.php
@@ -34,7 +34,7 @@ $m_table=$wpdb->prefix."adverts";
 <div class="box">
 	<h1>Latest Radio Shows 
     <div class="redsort">
-            <select name="dropdown" id="sel"  onclick="changeOpt()">
+            <select name="dropdown" id="sel"  onchange="changeOpt()">
                 <option value="latest" <?php if($param_1 == "latest" ){ echo 'selected="selected"';} ?> >Latest</option>
                 <option value="bycategories" <?php if($param_1 == "bycategories" ){ echo 'selected="selected"';} ?> >By Categories</option>
                 <option value="byshows" <?php if($param_1 == "byshows" ){ echo 'selected="selected"';} ?> >By Shows</option>
@@ -91,7 +91,7 @@ function changeOpt(){
 							'posts_per_page'   => 10,
 							'post_type'        => 'radio-articles',
 							'radio-shows' 	  => $am,
-							'order'			=> 'DESC',
+							'order'			=> 'ASC',
 							'post_status'      => 'publish',
 							'paged' 			=> $current_page,
 							);			
@@ -140,49 +140,7 @@ function changeOpt(){
 			echo '</div>';
 ?>
 <?php
-
-function custom_pagination_r($numpages = '', $pagerange = '', $paged='') {
-   if (empty($pagerange)) {
-    $pagerange = 2;
-  }
-  global $paged;
-  if (empty($paged)) {
-    $paged = 1;
-  }
-  if ($numpages == '') {
-    global $wp_query;
-    $numpages = $wp_query->max_num_pages;
-    if(!$numpages) {
-        $numpages = 1;
-    }
-  }
-  $pagination_args = array(
-    'base'            => get_pagenum_link(2) . '%_%',
-    'format'          => 'page/%#%',
-    'total'           => $numpages,
-    'current'         => $paged,
-    'show_all'        => False,
-    'end_size'        => 1,
-    'mid_size'        => $pagerange,
-    'prev_next'       => True,
-    'prev_text'       => __('&laquo;'),
-    'next_text'       => __('&raquo;'),
-    'type'            => 'plain',
-    'add_args'        => false,
-    'add_fragment'    => ''
-  );
-  $paginate_links = paginate_links($pagination_args);
-  if ($paginate_links) {
-    echo "<nav class='custom-pagination'>";
-      echo "<span class='page-numbers page-num'>Page " . $paged . " of " . $numpages . "</span> ";
-      echo $paginate_links;
-    echo "</nav>";
-  }
-}
-
-      
-        custom_pagination_r($loop->max_num_pages,"",$current_page);
-      
+wp_pagenavi( array( 'query' => $loop ) );
 ?>
 </div>
 <?php get_footer();?>
