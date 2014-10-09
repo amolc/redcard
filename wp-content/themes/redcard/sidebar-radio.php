26f6fe7e7d89d06d1b835aa9d1ddc4ee640b9966
amaninfodreams <aman.infodreams@gmail.com>
2014-10-09 11:31:12 +0530

    15.2.2

--- wp-content/themes/redcard/sidebar-radio.php
+++ wp-content/themes/redcard/sidebar-radio.php
@@ -7,8 +7,23 @@
  */
 ?>
 <div class="right">
-  <h2></h2>
-  <div class="ad_right" align="center"><?php dynamic_sidebar( 'postsponser' ); ?><label style="font-size:10px;">Advertisement</label></div>
+  <?php
+ $m_table=$wpdb->prefix."adverts";
+	$advertQuery="select * from $m_table where page='radio-shows' and isactive='1' order by adId DESC LIMIT 0,1";
+	$advertSql=$wpdb->get_results($advertQuery);
+	
+  if(sizeof($advertSql)>0)
+	{
+		foreach($advertSql as $adsql)
+		{
+		?>
+		  <h2></h2>
+ <div class="ad_right" align="center"><a href="<?php echo urldecode($adsql->adlink2);?>" target="_blank"><img width="302" height="252" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage2;?>" /></a><label style="font-size:10px;">Advertisement</label>
+</div>
+
+		<?php
+		}
+	} ?>
   <h2>Popular Audios</h2>
   <div class="popular">
     <ul>
