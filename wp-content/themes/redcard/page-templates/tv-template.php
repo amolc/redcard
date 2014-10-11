42779adf7cc16b5b61a7d10f9f59eae9a975bd68
amaninfodreams <aman.infodreams@gmail.com>
2014-10-11 17:06:37 +0530

    20.6

--- wp-content/themes/redcard/page-templates/tv-template.php
+++ wp-content/themes/redcard/page-templates/tv-template.php
@@ -107,7 +107,9 @@ function changeOpt(){
       <div class="message"></div>
       </a>
 	<?php		
-			echo '</div><span>'.getPostViews($postID).'<span></span></span></div>';
+			//echo '</div><span>'.getPostViews($postID).'<span></span></span></div>';
+			
+			echo '</div><span><span></span></span></div>';
    			$i++;
 			endwhile;
 			echo '</div>';
