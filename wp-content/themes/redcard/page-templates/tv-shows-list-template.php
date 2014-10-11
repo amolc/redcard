5a12e6b6c2dcdde803e3eb90212b6f41841912ed
amaninfodreams <aman.infodreams@gmail.com>
2014-10-11 16:07:14 +0530

    20.2

--- wp-content/themes/redcard/page-templates/tv-shows-list-template.php
+++ wp-content/themes/redcard/page-templates/tv-shows-list-template.php
@@ -56,7 +56,7 @@ $termlink = get_term_link($term);
           </a>
           <p><?php echo $term->description; ?></p>
           <div style="clear:both;"></div>
-		  <div style="background: #333;width: 45px;"><a style="color: #fff;font-size: 12px;font-weight: bold;padding: 2px 5px;" href="<?php echo $URL_values; ?>">Listen</a></div>
+		  <div style="background: #333;width: 45px;"><a style="color: #fff;font-size: 12px;font-weight: bold;padding: 2px 5px;" href="<?php echo $termlink; ?>">Watch</a></div>
         </div>
         <?php
 		?>
