37f194976612906a8548177cd063f712624f9490
amaninfodreams <aman.infodreams@gmail.com>
2014-10-14 17:55:52 +0530

    23.0

--- wp-content/themes/redcard/header.php
+++ wp-content/themes/redcard/header.php
@@ -240,7 +240,7 @@ if(is_front_page()){
         <?php $trimcontent = get_the_content();
 		  $youtubtagline_value = get_post_meta( $post->ID, '_cmb_footballs_tagline_text', true ); 
 		//  $shortcontent = wp_trim_words( $youtubtagline_value, $num_words = 10, $more = '…' ); ?>
-          <p style="height:90px;overflow:hidden;"><?php echo $youtubtagline_value; ?></p>
+          <p style="height:90px; margin-bottom:0;overflow:hidden;"><?php echo $youtubtagline_value; ?></p>
         <div class="date" id="date2"> <span>
 		
 		<?php //echo getPostViews(get_the_ID());?></span>
