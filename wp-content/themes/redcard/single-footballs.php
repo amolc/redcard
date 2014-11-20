3b54be95fa3cadb0cf76901222389649fd0273a7
amaninfodreams <aman.infodreams@gmail.com>
2014-11-20 14:32:19 +0530

    SF_2

--- wp-content/themes/redcard/single-footballs.php
+++ wp-content/themes/redcard/single-footballs.php
@@ -1,149 +1,143 @@
 <?php
-
-
-get_header(); 
-$m_table=$wpdb->prefix."adverts";
+	get_header(); 
+	$m_table=$wpdb->prefix."adverts";
 	$advertQuery="select * from $m_table where page='football-articles' and isactive='1' order by adId DESC LIMIT 0,1";
 	$advertSql=$wpdb->get_results($advertQuery);
-	
 	if(sizeof($advertSql)>0)
 	{
 		foreach($advertSql as $adsql)
 		{
-		?>
-		
+?>
 <div class="ad_1" align="center"><a href="<?php echo urldecode($adsql->adlink1);?>" target="_blank"><img width="731" height="93" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage1;?>" /></a>
-<label style="font-size:9px;">ADVERTISEMENT</label>
+  <label style="font-size:9px;">ADVERTISEMENT</label>
 </div>
-
-		<?php
+<?php
 		}
 	}
 ?>
-  <?php $postID = get_the_ID(); 
-						
-						  $mytermArray = array();
-                          $term_list_reg = wp_get_post_terms($postID, 'footballregions');  
-						  $term_list_exc = wp_get_post_terms($postID, 'footballexclusive'); 
-						  $term_list_opi = wp_get_post_terms($postID, 'footballopinion');  
-						  $term_list_week = wp_get_post_terms($postID, 'footballintheweek'); 
-						  $as =1;
-						
-						  if(!empty($term_list_reg))
-						  {
-							  foreach($term_list_reg as $row)
-							  {
-								  $mytermArray[$as]['term_id']= $row->term_id;
-								  $mytermArray[$as]['name']= $row->name;
-								  $mytermArray[$as]['slug']= $row->slug;
-								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
-								  $mytermArray[$as]['link']= get_term_link( $row );
-								 
-								  $as++;
-							  }
-						  }
-						  if(!empty($term_list_exc))
-						  {
-							  foreach($term_list_exc as $row)
-							  {
-								  $mytermArray[$as]['term_id']= $row->term_id;
-								  $mytermArray[$as]['name']= $row->name;
-								  $mytermArray[$as]['slug']= $row->slug;
-								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
-								  $mytermArray[$as]['link']= get_term_link( $row );
-								  $as++;
-							  }
-						  }
-						  if(!empty($term_list_opi))
-						  {
-							  foreach($term_list_opi as $row)
-							  {
-								  $mytermArray[$as]['term_id']= $row->term_id;
-								  $mytermArray[$as]['name']= $row->name;
-								  $mytermArray[$as]['slug']= $row->slug;
-								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
-								  $mytermArray[$as]['link']= get_term_link( $row );
-								  $as++;
-							  }
-						  }
-						   if(!empty($term_list_week))
-						  {
-							  foreach($term_list_week as $row)
-							  {
-								  $mytermArray[$as]['term_id']= $row->term_id;
-								  $mytermArray[$as]['name']= $row->name;
-								  $mytermArray[$as]['slug']= $row->slug;
-								  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
-								  $mytermArray[$as]['link']= get_term_link( $row );
-								  $as++;
-							  }
-						  }
-						 
-						    ?>
-
+<?php 	$postID = get_the_ID();
+		  $mytermArray = array();
+		  $term_list_reg = wp_get_post_terms($postID, 'footballregions');  
+		  $term_list_exc = wp_get_post_terms($postID, 'footballexclusive'); 
+		  $term_list_opi = wp_get_post_terms($postID, 'footballopinion');  
+		  $term_list_week = wp_get_post_terms($postID, 'footballintheweek'); 
+		  $as =1;
+		  if(!empty($term_list_reg))
+		  {
+			  foreach($term_list_reg as $row)
+			  {
+				  $mytermArray[$as]['term_id']= $row->term_id;
+				  $mytermArray[$as]['name']= $row->name;
+				  $mytermArray[$as]['slug']= $row->slug;
+				  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
+				  $mytermArray[$as]['link']= get_term_link( $row );
+				 
+				  $as++;
+			  }
+		  }
+		  if(!empty($term_list_exc))
+		  {
+			  foreach($term_list_exc as $row)
+			  {
+				  $mytermArray[$as]['term_id']= $row->term_id;
+				  $mytermArray[$as]['name']= $row->name;
+				  $mytermArray[$as]['slug']= $row->slug;
+				  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
+				  $mytermArray[$as]['link']= get_term_link( $row );
+				  $as++;
+			  }
+		  }
+		  if(!empty($term_list_opi))
+		  {
+			  foreach($term_list_opi as $row)
+			  {
+				  $mytermArray[$as]['term_id']= $row->term_id;
+				  $mytermArray[$as]['name']= $row->name;
+				  $mytermArray[$as]['slug']= $row->slug;
+				  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
+				  $mytermArray[$as]['link']= get_term_link( $row );
+				  $as++;
+			  }
+		  }
+		   if(!empty($term_list_week))
+		  {
+			  foreach($term_list_week as $row)
+			  {
+				  $mytermArray[$as]['term_id']= $row->term_id;
+				  $mytermArray[$as]['name']= $row->name;
+				  $mytermArray[$as]['slug']= $row->slug;
+				  $mytermArray[$as]['term_taxonomy_id']= $row->term_taxonomy_id;
+				  $mytermArray[$as]['link']= get_term_link( $row );
+				  $as++;
+			  }
+		  }
+?>
 <div id="container">
-
   <div class="left">
-    <?php
-				while ( have_posts() ) : the_post();
-				?>
-      <h2>Football > <?php echo $mytermArray[1]['name']; ?></h2>
-      <div style="border: 1px solid rgb(137, 137, 137);"></div>
-      <div style="clear:both; margin-bottom: 10px;"></div>
-    <span class="bywriter" style="margin-left: 0px;"><?php the_time('l, F j, Y'); ?></span>
-   <div class="sigle-football-title" style="margin-bottom: 13px;"><h1 style="margin-bottom: 0px ! important;margin-top: 7px !important;"><?php the_title(); ?></h1></div>
-    <?php $postID = get_the_ID(); ?>
-    <?php
+    <?php	while ( have_posts() ) : the_post();	?>
+    <h2>Football > <?php echo $mytermArray[1]['name']; ?></h2>
+    <div style="border: 1px solid rgb(137, 137, 137);"></div>
+    <div style="clear:both; margin-bottom: 10px;"></div>
+    <span class="bywriter" style="margin-left: 0px;">
+    <?php the_time('l, F j, Y'); ?>
+    </span>
+    <div class="sigle-football-title" style="margin-bottom: 13px;">
+      <h1 style="margin-bottom: 0px ! important;margin-top: 7px !important;">
+        <?php the_title(); ?>
+      </h1>
+    </div>
+    <?php $postID = get_the_ID(); 
 		  $youtubtagline_value = get_post_meta( $postID, '_cmb_footballs_tagline_text', true );  ?>
-          <span style="font-weight: bold;  background: none repeat scroll 0 0 yellow;"><?php echo $youtubtagline_value; ?></span>
-          <div style="clear:both; margin-bottom: 10px;"></div>
-         <span class="bywriter" style=" margin-left:0;">By <?php the_author_meta('display_name'); ?> <a href="<?php the_author_meta('twitter'); ?>" target="_blank" >(@<?php the_author_meta('facebook'); ?>)</a></span>
-		   
-   
+    <span style="font-weight: bold;  background: none repeat scroll 0 0 yellow; font-size: 16px;"><?php echo $youtubtagline_value; ?></span>
+    <div style="clear:both; margin-bottom: 10px;"></div>
+    <span class="bywriter" style=" margin-left:0;">By
+    <?php the_author_meta('display_name'); ?>
+    <a href="<?php the_author_meta('twitter'); ?>" target="_blank" >(@
+    <?php the_author_meta('facebook'); ?>
+    )</a></span>
     <div class="date" >
-
-     
-      <div id="social_3">
-         <a onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;" href="http://www.facebook.com/share.php?u=<?php echo urlencode(get_permalink( $article->ID));?>&description=<?php echo the_title();?>"  title="Share on Facebook" ><div class="facebook" ></div></a>
-        <a href="http://twitter.com/intent/tweet?text=<?php the_title();?> <?php echo get_permalink( $article->ID);?> via @RedCardConnect"  ><div class="twitter"></div></a>
-        <a  href="<?php echo get_permalink( $article->ID);?>#dis_comment"><div class="message"></div></a>
-        
-      </div>
+      <div id="social_3"> <a onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;" href="http://www.facebook.com/share.php?u=<?php echo urlencode(get_permalink( $article->ID));?>&description=<?php echo the_title();?>"  title="Share on Facebook" >
+        <div class="facebook" ></div>
+        </a> <a href="http://twitter.com/intent/tweet?text=<?php the_title();?> <?php echo get_permalink( $article->ID);?> via @RedCardConnect"  >
+        <div class="twitter"></div>
+        </a> <a  href="<?php echo get_permalink( $article->ID);?>#dis_comment">
+        <div class="message"></div>
+        </a> </div>
     </div>
     <div class="post">
-    
       <div class="single-post-image">
         <?php twentyfourteen_post_thumbnail(); ?>
-            <div style="margin-bottom: 10px; margin-top: -10px;"> <?php
-
-	 
-	  		if(!empty($mytermArray)){
-
-						 $is =1;
-						 $arraycount =  count($mytermArray);
-						  foreach($mytermArray as $row){
-							  
-							  	if($is == $arraycount)
-								{
-	?>
-    							<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>
-    							<?php } else { ?>
-    											<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>,
-    									<?php } ?>
-                                <?php $is++; ?>
-                                <?php } } ?></div>
-		<?php the_content(); ?>
+        <div style="margin-bottom: 10px; margin-top: -10px;">
+          <?php
+          		if(!empty($mytermArray))
+				{
+					$is =1;
+					$arraycount =  count($mytermArray);
+					foreach($mytermArray as $row)
+					{
+					if($is == $arraycount)
+					{
+		  ?>
+          				<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>
+          <?php 	}
+		  			else
+					{
+		  ?>
+          				<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>,
+          <?php 	} 
+					$is++;
+					}
+				} ?>
+        </div>
+        <?php the_content(); ?>
       </div>
-      <?php
-				endwhile;
-			?>
+      <?php endwhile; ?>
     </div>
     <?php
-	
-			  $mytermArray = array();
+		  $mytermArray = array();
 		  $term_list_tag = wp_get_post_terms($postID, 'footballtags');
 		  $as =1;
-		   if(!empty($term_list_tag))
+	      if(!empty($term_list_tag))
 						  {
 							  foreach($term_list_tag as $row)
 							  {
@@ -157,51 +151,62 @@ $m_table=$wpdb->prefix."adverts";
 						  }
 	
 	 ?>
- 
-    <div class="tags"> <strong>Tags:</strong>      <?php if(!empty($mytermArray)){
+    <div class="tags"> <strong>Tags:</strong>
+      <?php if(!empty($mytermArray))
+	  					{
 						 $is =1;
 						 $arraycount =  count($mytermArray);
-						  foreach($mytermArray as $row){
+						  foreach($mytermArray as $row)
+						  {
 							  	if($is == $arraycount)
 								{
-	?>
-    							<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>
-    							<?php } else { ?>
-    											<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>,
-    									<?php } ?>
-                                <?php $is++; ?>
-                                <?php } } ?>	<span><a href="#dis_comment">Comments</a></span> </div>
-    
+		?>
+      							<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>
+      <?php 					}
+	  							else
+								{
+	 	?>
+      							<a href="<?php echo $row['link']?>"><?php echo $row['name']?></a>,
+      <?php 					}
+	  							$is++;
+							}
+						}
+		?>
+    <span><a href="#dis_comment">Comments</a></span> </div>
     <div class="author-bio">
-			<div class="authimage"><?php echo get_avatar( get_the_author_meta('email'), '90' ); ?></div>
-			<div class="author-info">
-				<h3 class="author-title"><?php the_author_link(); ?></h3>
-				<p class="author-description"><?php the_author_meta('description'); ?></p>
-			</div>
-	</div>
-	<div style="clear:both;"></div>
+      <div class="authimage"><?php echo get_avatar( get_the_author_meta('email'), '90' ); ?></div>
+      <div class="author-info">
+        <h3 class="author-title">
+          <?php the_author_link(); ?>
+        </h3>
+        <p class="author-description">
+          <?php the_author_meta('description'); ?>
+        </p>
+      </div>
+    </div>
+    <div style="clear:both;"></div>
     <h2>Related Articles</h2>
-    <?php $postid = get_the_ID(); ?>
     <?php 
-	$args = array(
-    'post__not_in '=> $postid,
-    'numberposts' => 2,
-    'orderby' => 'rand',          
-    'post_type' => 'footballs',
-    'post_status' => 'publish');
-
-    $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
-   
+		$postid = get_the_ID();
+		$args = array(
+		'post__not_in '=> $postid,
+		'numberposts' => 2,
+		'orderby' => 'rand',          
+		'post_type' => 'footballs',
+		'post_status' => 'publish');
+		$recent_posts = wp_get_recent_posts( $args, ARRAY_A );
     ?>
     <div class="related">
       <ul>
-        <?php if(!empty($recent_posts)){ 
-              foreach($recent_posts as $row){
-                  
-                   $feat_image = wp_get_attachment_url( get_post_thumbnail_id($row['ID']) );
-              ?>
-        <li> <img src="<?php  echo $feat_image;?>"> <a href="<?php echo get_permalink( $row['ID']); ?>"><?php echo $row['post_title']; ?></a> </li>
-        <?php } } ?>
+        <?php if(!empty($recent_posts))
+				{ 
+				  foreach($recent_posts as $row)
+				  {
+				  $feat_image = wp_get_attachment_url( get_post_thumbnail_id($row['ID']) );
+        ?>
+        		  <li> <img src="<?php  echo $feat_image;?>"> <a href="<?php echo get_permalink( $row['ID']); ?>"><?php echo $row['post_title']; ?></a> </li>
+        <?php 	}
+				} ?>
       </ul>
     </div>
     <h2 id="dis_comment">Comments</h2>
@@ -209,7 +214,6 @@ $m_table=$wpdb->prefix."adverts";
     <script type="text/javascript">
         /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * mekey * */
         var disqus_shortname = 'redcardconnect'; // required: replace example with your forum shortname
-
         /* * * DON'T EDIT BELOW THIS LINE * * */
         (function() {
             var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
@@ -221,44 +225,31 @@ $m_table=$wpdb->prefix."adverts";
     Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a>
     </noscript>
   </div>
+  <?php setPostViews(get_the_ID());?>
+  <div class="right">
   <?php
-  setPostViews(get_the_ID());
-  
- ?>
- <div class="right">
- 
-  <?php
-  $m_table=$wpdb->prefix."adverts";
+    $m_table=$wpdb->prefix."adverts";
 	$advertQuery="select * from $m_table where page='football-articles' and isactive='1' order by adId DESC LIMIT 0,1";
 	$advertSql=$wpdb->get_results($advertQuery);
-	
 	if(sizeof($advertSql)>0)
 	{
 		foreach($advertSql as $adsql)
 		{
-			?>
-			 <h2>&nbsp;</h2>
-			  <div class="ad_right" align="center">
-              <a href="<?php echo urldecode($adsql->adlink2);?>" target="_blank"><img width="302" height="252" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage2;?>" /></a>
-              <label style="font-size:10px;">ADVERTISEMENT</label>
-              </div>
-			<?php
+	?>
+    <h2>&nbsp;</h2>
+    <div class="ad_right" align="center"> <a href="<?php echo urldecode($adsql->adlink2);?>" target="_blank"><img width="302" height="252" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage2;?>" /></a>
+      <label style="font-size:10px;">ADVERTISEMENT</label>
+    </div>
+    <?php
 		}
 	}
-		?>
-  
-  
-  <h2>Popular Posts</h2>
-  <div class="popular">
-    <ul>
-      <?php /* For Popular Posts*/ if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('Sidebar 1') ) ?>
-     
-    </ul>
+	?>
+    <h2>Popular Posts</h2>
+    <div class="popular">
+      <ul>
+        <?php /* For Popular Posts*/ if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('Sidebar 1') ) ?>
+      </ul>
+    </div>
   </div>
- 
 </div>
-
-</div>
-<?php get_footer();
-
-?>
+<?php get_footer(); ?>
