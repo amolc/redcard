60edb3babd35123d9e8a31967c6ed1638253c345
amolv <amolvhankalas@gmail.com>
2014-11-23 10:00:09 +0530

    hader ch

--- wp-content/themes/redcard/template-suzuki-cup.php
+++ wp-content/themes/redcard/template-suzuki-cup.php
@@ -2,44 +2,10 @@
 /*
 	Template Name: Suzuki Cup Template
 */
-//get_header(); ?>
-<!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) & !(IE 8)]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico" type="image/x-icon"/>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
-<![endif]-->
-<link href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.min.css" rel="stylesheet">
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/slider/jquery.min.js"></script>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/prettyphoto/css/prettyPhoto.css" type="text/css" media="screen" charset="utf-8" />
-<script src="<?php echo get_template_directory_uri(); ?>/prettyphoto/js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/jcarousel/style.css" type="text/css" media="screen" />
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/jcarousel/js/jcarousellite_1.0.1.pack.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/jcarousel/js/captify.tiny.js"></script>
-<?php wp_head(); ?>
-</head>
+get_header(); ?>
 
-<body>
 
- <div id="header">
+ <div id="suzuki-header">
 	<div class="baba">
 		<a href="">#RedCardConnect</a>
 		<a href="">#AFFSuzukiCup</a>
@@ -59,19 +25,14 @@
 	<img src="/suzuki-images/SuzukiCupiframe-900pxlwidth.jpg" style="width:600px;height:150px;margin:20px 30px">
 </div>
 
-<div id="social" class="right top">
-      </div>
- 
-</div>
 <style>
 .main{height: 28px; color: black; background: URL('/suzuki-images/slice.png'); margin-top: 10px; width: 669px;}
 .suzuki-menu{ float: right;}
-#header{ width: 950px;
-height: 290px;
-display: block;
+#suzuki-header{ 
+background-size: 100%;
 overflow: hidden;
 margin: 0 auto;
-background: URL('/suzuki-images/r1.jpg'); }
+background: URL('/suzuki-images/r1.jpg') 0 0; }
 
 .news{ width: 50; display:block; background: URL('/suzuki-images/news.png'); }
 .dada{width:50px; height:30px; display:block; overflow:hidden; margin:0 auto; }
