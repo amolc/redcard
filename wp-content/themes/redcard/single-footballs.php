0820b71f28f43c50fbfcb0cc4c2708e26a6674b5
amaninfodreams <aman.infodreams@gmail.com>
2014-11-25 17:06:17 +0530

    short url for twitter on football posts

--- wp-content/themes/redcard/single-footballs.php
+++ wp-content/themes/redcard/single-footballs.php
@@ -96,7 +96,7 @@
     <div class="date social-bar" style="margin-top: 10px;" >
       <div id="social_3"> <a onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400');return false;" href="http://www.facebook.com/share.php?u=<?php echo urlencode(get_permalink( $article->ID));?>&description=<?php echo the_title();?>"  title="Share on Facebook" >
         <div class="facebook" ></div>
-        </a> <a href="http://twitter.com/intent/tweet?text=<?php the_title();?> <?php echo get_permalink( $article->ID);?> via @RedCardConnect"  >
+        </a> <a href="http://twitter.com/intent/tweet?text=<?php the_title();?> <?php echo wp_get_shortlink( $article->ID);?> via @RedCardConnect"  >
         <div class="twitter"></div>
         </a> <a  href="<?php echo get_permalink( $article->ID);?>#dis_comment">
         <div class="message"></div>
