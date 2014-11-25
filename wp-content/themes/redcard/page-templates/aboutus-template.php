239a1342f1df2698f82272fb6d8e261b2998dab0
amaninfodreams <aman.infodreams@gmail.com>
2014-11-25 12:36:30 +0530

    about us template

---
+++ wp-content/themes/redcard/page-templates/aboutus-template.php
@@ -0,0 +1,15 @@
+<?php /* Template Name: About Us Template Page*/
+get_header();
+
+
+the_block('Our Services');
+
+the_block('Advertise With Us');
+
+the_block('Join Our Team');
+
+the_block('Connect With Us');
+
+get_footer();
+/*Aman*/
+?>
