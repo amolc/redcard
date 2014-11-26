2f815eb4097adbedbc054a3b4588dc906a896625
amaninfodreams <aman.infodreams@gmail.com>
2014-11-26 18:20:48 +0530

    count-tw-3

--- wp-content/themes/redcard/single-footballs.php
+++ wp-content/themes/redcard/single-footballs.php
@@ -112,6 +112,7 @@
         <a href="http://twitter.com/intent/tweet?text=<?php the_title();?> <?php echo wp_get_shortlink( $article->ID);?> via @RedCardConnect"  ><div class="twitter"></div></a> 
         </div>
         <a  href="<?php echo get_permalink( $article->ID);?>#dis_comment"><div class="message"></div></a>
+        <a href="<?php echo get_permalink( $article->ID);?>#disqus_thread" data-disqus-identifier="article_1_identifier">First article</a>
        </div>
     </div>
     <div class="post">
