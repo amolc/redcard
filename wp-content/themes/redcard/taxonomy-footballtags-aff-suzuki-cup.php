1631936c55dc224b1526ff8e32edc0d46e320161
amaninfodreams <aman.infodreams@gmail.com>
2014-11-26 10:30:26 +0530

    ads on suzuki-2

--- wp-content/themes/redcard/taxonomy-footballtags-aff-suzuki-cup.php
+++ wp-content/themes/redcard/taxonomy-footballtags-aff-suzuki-cup.php
@@ -225,7 +225,7 @@ $ad =1;
 			if($adsql->adimage1){
 		?>
 		
-<div class="ad_1" align="center" style="margin-bottom: 2px;"><a href="<?php echo urldecode($adsql->adlink1);?>" target="_blank"><img width="731" height="93" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage1;?>" /></a>
+<div class="ad_1" align="center" style=" margin-bottom: 15px;margin-top: 20px;"><a href="<?php echo urldecode($adsql->adlink1);?>" target="_blank"><img width="731" height="93" alt="" class="attachment-full" style="max-width: 100%;" src="<?php echo plugins_url();?>/advertisement/<?php echo $adsql->adimage1;?>" /></a>
 <label style="font-size:9px;">ADVERTISEMENT</label>
 </div>
 
